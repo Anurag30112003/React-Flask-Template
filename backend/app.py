628b156eb7622fc18225cc2a9fd1993992d38701
Anurag30112003 <anuragsharma011011@gmail.com>
2022-01-25 05:04:47 +0530

    flask backend

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+@app.route('/variable')
+def get_variabel():
+    return {'variable': 'helloworld from flask'}
--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import React ,{useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [variable,setVariable] = useState(0);
+
+  useEffect(()=>{
+    fetch('/variable').then(res=>res.json()).then(data=>{
+      setVariable(data.variable);
+    });
+  },[]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{variable}</p>
       </header>
     </div>
   );
